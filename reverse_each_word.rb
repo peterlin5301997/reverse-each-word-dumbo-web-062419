6a58905a845a3b97eef4a3120979eebd250546de
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 17:58:24 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(phrase)
   array = phrase.split
   reversed = ""
-  array.each do |word|
+  array.collect do |word|
     reversed += word.reverse
     if word != array.last
       reversed += " "
